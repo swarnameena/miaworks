7d4cec8888809c826e6d4a91cdc23b76150981aa
meena-gs <meena@gigaspaces.com>
2018-10-22 04:26:10 -0400

    additional model changes

---
+++ mia_profile_service/src/main/java/com/mia/profile/controller/impl/UserProfileControllerImpl.java
@@ -0,0 +1,17 @@
+package com.mia.profile.controller.impl;
+
+import com.mia.profile.controller.UserProfileController;
+import com.mia.profile.model.BasicUserPreference;
+import com.mia.profile.model.UserMoodMeal;
+
+import javax.xml.ws.Response;
+
+public class UserProfileControllerImpl implements UserProfileController {
+    public Response<BasicUserPreference> saveBasicUserPreference(BasicUserPreference basicUserPreference) {
+        return null;
+    }
+
+    public Response<UserMoodMeal> saveUserMoodMealPreference(UserMoodMeal userMoodMeal) {
+        return null;
+    }
+}
---
+++ mia_profile_service/src/main/java/com/mia/profile/dao/impl/UserProfileDaoImpl.java
@@ -0,0 +1,17 @@
+package com.mia.profile.dao.impl;
+
+import com.mia.profile.dao.UserProfileDao;
+import com.mia.profile.model.BasicUserPreference;
+import com.mia.profile.model.UserMoodMeal;
+
+import javax.xml.ws.Response;
+
+public class UserProfileDaoImpl implements UserProfileDao {
+    public Response<BasicUserPreference> saveBasicUserPreference(BasicUserPreference basicUserPreference) {
+        return null;
+    }
+
+    public Response<UserMoodMeal> saveUserMoodMealPreference(UserMoodMeal userMoodMeal) {
+        return null;
+    }
+}
